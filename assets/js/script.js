a2f70e7de1c4947455fb3615ff7dd91c0542d9bf
lilianaba <52921112+lilianaba@users.noreply.github.com>
2023-03-26 21:55:51 +1030

    forecast working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,107 @@
-var userFormEl = document.querySelector('#user-form');
-var currentweather = document.querySelector('.currentweather');
-var cityList = document.querySelector('#city-list');
+var userFormEl = document.querySelector("#user-form");
+var currentweather = document.querySelector(".currentweather");
+var cityList = document.querySelector("#city-list");
 // var forecast = document.querySelector('#forecast');
-var userCity = document.querySelector('#userCity');
-var forecastListEl = document.querySelector('#forecast-list');
-const apiKey = '5f95d7a679cab867a94961cf83fc403f';
+var userCity = document.querySelector("#userCity");
+var forecastListEl = document.querySelector("#forecast-list");
+const apiKey = "5f95d7a679cab867a94961cf83fc403f";
 
-const displayCurrentWeather=(data)=>{
-    let dateEl = document.createElement("h2");
-    let cityEl = document.createElement("h2");
-    let tempEl = document.createElement("p");
-    let humidityEl = document.createElement("p");
-    let windSpeedEl= document.createElement("p");
-    let icon = document.createElement("img");
-    dateEl.innerText = (new Date().toLocaleDateString());
-    cityEl.innerText = (data.name);
-    tempEl.innerText = ("The Temperature: " + data.main.temp);
-    humidityEl.innerText = ("Humidity: " +data.main.humidity + "%");
-    windSpeedEl.innerText = ("Windy speed:  "+data.wind.speed);
-    icon.setAttribute("src","https://openweathermap.org/img/w/"+data.weather[0].icon+".png")
+const displayCurrentWeather = (data) => {
+  currentweather.innerText = "";
+  let dateEl = document.createElement("h2");
+  let cityEl = document.createElement("h2");
+  let tempEl = document.createElement("p");
+  let humidityEl = document.createElement("p");
+  let windSpeedEl = document.createElement("p");
+  let icon = document.createElement("img");
+  dateEl.innerText = new Date().toLocaleDateString();
+  cityEl.innerText = data.name;
+  tempEl.innerText = "The Temperature: " + data.main.temp+ " °C";
+  humidityEl.innerText = "Humidity: " + data.main.humidity + "%";
+  windSpeedEl.innerText = "Windy speed:  " + data.wind.speed;
+  icon.setAttribute(
+    "src",
+    "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+  );
 
+  currentweather.append(dateEl, cityEl, tempEl, humidityEl, windSpeedEl, icon);
+};
 
+const forecast = (city) => {
+  forecastListEl.innerText = "";
+  var apiUrlWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
-    currentweather.append(dateEl,cityEl,tempEl,humidityEl,windSpeedEl, icon);
-
-}
-
-const forecast=(city) =>{
-  var apiUrlWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
-
-  fetch(apiUrlWeather)
-  .then(function (response) {
-
-      if (response.ok) {
-        response.json().then(function (data) {
-        for (var i = 0; i < data.list.length; i++) {
-          if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-          
-          console.log(data);
+  fetch(apiUrlWeather).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
      
-          displayCurrentWeather(data);
-            }}})}})    };
-  
-const fetchCurrentWeather =(city) =>{
-    var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=metric`
-    fetch(apiUrlWeather)
+        for (var i = 0; i < data.list.length; i++) {
+          // let divEl=document.createElement("div");
+          // divEl.classList.add("day");
+          if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+            console.log(data);
+            let divEl=document.createElement("div");
+            let dateEl = document.createElement("h2");
+            let cityEl = document.createElement("h2");
+            let tempEl = document.createElement("p");
+            let humidityEl = document.createElement("p");
+            let windSpeedEl = document.createElement("p");
+            let icon = document.createElement("img");
+            dateEl.innerText = new Date(data.list[i].dt_txt).toLocaleDateString();
+            cityEl.innerText = data.city.name;
+            tempEl.innerText = "The Temperature: " + data.list[i].main.temp +" °C";
+            humidityEl.innerText = "Humidity: " + data.list[i].main.humidity + "%";
+            windSpeedEl.innerText = "Windy speed:  " +data.list[i].wind.speed;
+            icon.setAttribute(
+              "src",
+              "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
+            );
+          
+            divEl.append(dateEl, cityEl, tempEl, humidityEl, windSpeedEl, icon);
+            forecastListEl.append(divEl);
+          }  
+         
+        }
+      });
+    }
+  });
+};
+
+const fetchCurrentWeather = (city) => {
+  var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=metric`;
+  fetch(apiUrlWeather)
     .then(function (response) {
       if (response.ok) {
-          response.json().then(function (data) {
+        response.json().then(function (data) {
           console.log(data);
-     
-          displayCurrentWeather(data);
-          forecast(city);
-          createRow(data.name)
 
+          displayCurrentWeather(data);
+          createRow(data.name);
         });
       } else {
-        alert('Error: ' + response.statusText);
+        alert("Error: " + response.statusText);
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to weather API');
+      alert("Unable to connect to weather API");
     });
-
-}
+};
 
 function createRow(city) {
-// cityList = []
-let cityIt = document.createElement("li");
+  // cityList = []
+  let cityIt = document.createElement("li");
 
-cityIt.innerText=(city)
+  cityIt.innerText = city;
   cityList.append(cityIt);
-  console.log("Requested city: " + city );
+  console.log("Requested city: " + city);
   console.log(cityList);
-
 }
-const searchCity = (event)=>{
-    event.preventDefault();
-    let search = userCity.value;
-    fetchCurrentWeather(search);
-    userCity.value = '';
+const searchCity = (event) => {
+  event.preventDefault();
+  let search = userCity.value;
+  fetchCurrentWeather(search);
+  forecast(search);
+  userCity.value = "";
+};
 
-
-}
-
-
-userFormEl.addEventListener('submit',searchCity);
+userFormEl.addEventListener("submit", searchCity);
